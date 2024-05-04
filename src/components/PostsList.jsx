5dacf555103fb92df3433c188df0fa216ac59c00
Hyoungwoo In <io21in@hanmail.net>
2024-05-05 07:44:03 +0900

    lecture_43

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,25 +1,11 @@
 import PropTypes from 'prop-types'; // 이렇게 정확하게 추가해야 합니다.
 import Post from './Post';
+import { useLoaderData } from 'react-router-dom';
 import classes from './PostsList.module.css';
-import { useState, useEffect } from 'react';
 
 function PostsList(){ 
-    const [posts, setPosts] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
-
-    useEffect(() => {
-        async function fetchPosts() {  
-            setIsFetching(true);  
-            const response = await fetch('http://localhost:8080/posts')
-            const resData = await response.json();
-            setPosts(resData.posts);
-            setIsFetching(false);
-          }
-
-          fetchPosts();
-    }, []);
-
-   
+    const posts = useLoaderData();
+       
     function addPostHandler(postData) {
         fetch('http://localhost:8080/posts', {
             method: 'POST',
@@ -33,21 +19,16 @@ function PostsList(){
 
     return (
         <>
-            {!isFetching && posts.length > 0 && (           
+            {posts.length > 0 && (           
                 <ul className={classes.posts }>
                     {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>
             )} 
-            {!isFetching && posts.length === 0 && (
+            {posts.length === 0 && (
                 <div style={{ textAlign: 'center', color: 'white'}}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
-            )}
-            {isFetching && (
-                <div style={{ textAlign: 'center', color: 'white'}}>
-                    <p>Loading posts...</p>
-                </div> 
             )}
         </>
     );
